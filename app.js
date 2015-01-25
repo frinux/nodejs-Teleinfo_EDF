736801d4ff92674eeda44fc9ebe8d7e2d1f575e8
frinux <francois@frinux.fr>
2015-01-25 11:47:23 +0100

    update : better interface, bug correction, date filter now works

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var teleinfo = require('teleinfo');
 var util = require('util');
 var mysql = require('mysql');
-var app = require('express')();
+var express = require('express'), app = express();
+var bodyParser = require('body-parser')
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
@@ -16,50 +17,87 @@ function db() {
 //Use EJS rather then Jade templating
 app.set('view engine', 'ejs');
 
+//Parse JSON-encoded bodies (Ajax calls)
+app.use(bodyParser());
+
 //Routing
 app.get('/', function (req, res) {
-  res.render('live');
+  res.render('live', {'menu_item_active':'live'});
 })
 
 app.get('/graph_daily', function (req, res) {
-  res.render('graph_daily');
+  res.render('graph_daily', {'menu_item_active':'graph_daily'});
 })
 
 app.get('/graph_hourly', function (req, res) {
-  res.render('graph_hourly');
+  res.render('graph_hourly', {'menu_item_active':'graph_hourly'});
 })
 
 app.post('/graph_daily', function (req, res) {
 
 	var connection = db();
-	connection.query(' \
+
+	//MySQL query build
+	var query = ' \
 		SELECT UNIX_TIMESTAMP(CONCAT(YEAR, "-", MONTH, "-", DAY, " ", "23:59:59"))*1000 AS x, \
 		SUM_HCHC_DELTA AS y1, \
 		SUM_HCHP_DELTA AS y2, \
 		IF(DAYOFWEEK(CONCAT(YEAR, "-", MONTH, "-", DAY))=1 OR DAYOFWEEK(CONCAT(YEAR, "-", MONTH, "-", DAY))=7,"wetrue","wefalse") AS isweekend \
-		FROM daily_consumption', function(err, rows){
+		FROM daily_consumption \
+		WHERE 1=1';
+	if (req.body.from_date.length > 0 ) {
+		var date = req.body.from_date.split("-");
+		query += ' AND UNIX_TIMESTAMP(CONCAT(YEAR, "-", MONTH, "-", DAY, " ", "23:59:59")) >= UNIX_TIMESTAMP("' + req.body.from_date + '")';
+	}
+	if (req.body.to_date.length > 0 ) {
+		var date = req.body.to_date.split("-");
+		query += ' AND UNIX_TIMESTAMP(CONCAT(YEAR, "-", MONTH, "-", DAY, " ", "23:59:59")) <= UNIX_TIMESTAMP("' + req.body.to_date + '")';
+	}
+	query += ';';
+	//console.log(query);
 
-			if(err)
-				console.log("Error Selecting : %s ",err );
+	//Query execution
+	connection.query(query, function(err, rows){
 
-			res.json(rows);
+		if(err) {
+			console.log("Error Selecting : %s ",err );
+		}
+
+		res.json(rows);
 	});
 })
 
 app.post('/graph_hourly', function (req, res) {
 
 	var connection = db();
-	connection.query(' \
+
+	//MySQL query build
+	var query = ' \
 		SELECT UNIX_TIMESTAMP(CONCAT(YEAR, "-", MONTH, "-", DAY, " ", HOUR, ":59:59"))*1000 AS x, \
 		SUM_HCHC_DELTA AS y1, \
 		SUM_HCHP_DELTA AS y2, \
 		IF(DAYOFWEEK(CONCAT(YEAR, "-", MONTH, "-", DAY))=1 OR DAYOFWEEK(CONCAT(YEAR, "-", MONTH, "-", DAY))=7,"wetrue","wefalse") AS isweekend \
-		FROM hourly_consumption', function(err, rows){
+		FROM hourly_consumption \
+		WHERE 1=1';
+	if (req.body.from_date.length > 0 ) {
+		var date = req.body.from_date.split("-");
+		query += ' AND UNIX_TIMESTAMP(CONCAT(YEAR, "-", MONTH, "-", DAY, " ", HOUR, ":59:59")) >= UNIX_TIMESTAMP("' + req.body.from_date + '")';
+	}
+	if (req.body.to_date.length > 0 ) {
+		var date = req.body.to_date.split("-");
+		query += ' AND UNIX_TIMESTAMP(CONCAT(YEAR, "-", MONTH, "-", DAY, " ", HOUR, ":59:59")) <= UNIX_TIMESTAMP("' + req.body.to_date + '")';
+	}
+	query += ';';
+	//console.log(query);
 
-			if(err)
-				console.log("Error Selecting : %s ",err );
+	//Query execution
+	connection.query(query, function(err, rows){
 
-			res.json(rows);
+		if(err) {
+			console.log("Error Selecting : %s ",err );
+		}
+
+		res.json(rows);
 	});
 })
 
